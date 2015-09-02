bcb0a18a43454fce764f092b188ecb2700e825c1
Kevin Schuetz <schuetzk@gmail.com>
2015-09-01 20:41:38 -0500

    Chrome, work in progress

--- src/app/components/Chrome.react.js
+++ src/app/components/Chrome.react.js
@@ -24,19 +24,82 @@
 
 
 const
-    React = require('react');
+    React = require('react'),
+    FlatX = require('./FlatX.react'),
+    FlatO = require('./FlatO.react');
 
+
+const PieceIcon = React.createClass({
+    render() {
+        const pieceClass = this.props.player === 2 ? FlatO : FlatX;
+        return React.DOM.svg(null,
+            React.DOM.g({
+                transform: 'scale(20)'
+            }, React.createElement(pieceClass)));
+    }
+});
+
+const PlayerRow = React.createClass({
+    render() {
+
+        const turnIndicator = React.DOM.div({
+            className:  'col-md-2'
+        }, '->');
+
+        const pieceCell = React.DOM.div({
+            className:  'col-md-2'
+        }, React.createElement(PieceIcon, { player: this.props.player }));
+
+        const playerNameCell = React.DOM.div({
+            className:  'col-md-8'
+        }, 'PLAYER');
+
+        return React.DOM.div({
+            className: 'row'
+        }, turnIndicator, pieceCell, playerNameCell);
+
+    }
+
+});
+
+
+const PlayerSwap = React.createClass({
+    render() {
+
+        return React.DOM.div({});
+    }
+});
+
+const PlayerGrid = React.createClass({
+    render() {
+        const player1Row = React.createElement(PlayerRow, { player: 1}),
+              player2Row = React.createElement(PlayerRow, { player: 2});
+
+        const playerRows = React.DOM.div({
+            className: 'col-md-8'
+        }, player1Row, player2Row);
+
+        const playerSwapCell = React.createElement({
+            className: 'col-md-4'
+        }, React.createElement(PlayerSwap, {}));
+
+        return React.DOM.div({
+            className: 'row'
+        }, playerRows, playerSwapCell);
+    }
+});
 
 const Chrome = React.createClass({
 
     render() {
 
+        const playerGrid = React.createElement(PlayerGrid);
+
+
 
         return React.DOM.div({
 
-        });
-
-
+        }, playerGrid);
 
     }
 });
